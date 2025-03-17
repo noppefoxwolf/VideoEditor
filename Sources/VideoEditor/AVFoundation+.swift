b47d3b5d82f0db58aac2053d4ca33ec7002a71f5
noppe <noppelabs@gmail.com>
2025-03-18 03:46:55 +0900

    refactor

---
+++ Sources/VideoEditor/AVFoundation+.swift
@@ -0,0 +1,26 @@
+@preconcurrency import AVKit
+
+extension AVAsset {
+    var fullRange: CMTimeRange {
+        get async throws {
+            let duration = try await load(.duration)
+            return CMTimeRange(start: .zero, duration: duration)
+        }
+    }
+    
+    func trimmedComposition(_ range: CMTimeRange) async throws -> AVAsset {
+        let fullRange = try await fullRange
+        guard CMTimeRangeEqual(fullRange, range) == false else { return self }
+        
+        let composition = AVMutableComposition()
+        try await composition.insertTimeRange(range, of: self, at: .zero)
+        
+        if let videoTrack = try await loadTracks(withMediaType: .video).first {
+            let preferredTransform = try await videoTrack.load(.preferredTransform)
+            composition.tracks.forEach {
+                $0.preferredTransform = preferredTransform
+            }
+        }
+        return composition
+    }
+}
